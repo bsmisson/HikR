26ff48823a3df2c3ca092601337cee663d1185fe
Maha Abdelnabi <mahaomar@gmail.com>
2023-02-06 23:42:00 -0500

    adding features to the pictures

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,20 @@
+// //photos API
+// var clienId = "VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
+// var endpoint =
+//   "https://api.unsplash.com/photos/random?client_id=VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
 
+// var imageEl = document.querySelector(".adventue-pic");
+// var imageLink = document.getElementById("imglink");
+
+
+// fetch(endpoint)
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(jsonData){
+//     imageEl.src = jsonData.urls.regular;
+//     imageLink.setAttribute("href", jsonData.links.html);
+// })
+// .catch(function(error){
+//     console.log("error: ",error);
+// });
