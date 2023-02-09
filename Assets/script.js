f9a9bc3dc4d08e91bdd2c963605547a879d604d0
bsmisson <bensmisson@gmail.com>
2023-02-09 14:04:37 -0500

    added button connection to API

--- Assets/script.js
+++ Assets/script.js
@@ -18,3 +18,26 @@
 // .catch(function(error){
 //     console.log("error: ",error);
 // });
+
+const api_key = "aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR";
+let stateName = document.getElementById('city-input'); 
+let searchEl = document.getElementById("mybtn");
+
+const requestURL = 'https://developer.nps.gov/api/v1/parks?parkCode=&stateCode='+ stateName.value +'&api_key=aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR';
+
+function findPark() {
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+searchEl.addEventListener("click", function() {
+    findPark();
+})
+// searchEl.onclick = findPark;
+console.log(searchEl);
+console.log(stateName.value);
