231cd9c8bd06b83ad669719017334d94d9abf9fb
valentinaaranggo <valea28@gmail.com>
2023-02-09 15:44:58 -0500

    added some api code find a park in any state

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,31 @@
-// //photos API
+const api_key= "0JZql2LooCmwKdSlgCcTL1TNSmRyqkyf2yDwKBzE"
+let cityName=document.getElementById('city');
+let searchE1= document.getElementById('myBtn');
+
+
+const requestUrl="https://developer.nps.gov/api/v1/parks?stateCode="+ cityName.value + "&limit=0&start=50&api_key=" + api_key;
+
+function findPark(){
+    fetch(requestUrl)  
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+}
+
+searchE1.addEventListener("click",function(){
+    findPark();
+})
+console.log(searchE1);
+
+
+
+
+
+
+//photos API
 // var clienId = "VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
 // var endpoint =
 //   "https://api.unsplash.com/photos/random?client_id=VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
