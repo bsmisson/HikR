8a758f6d7d1cd11d2608c510bfdf84045f3da253
bsmisson <bensmisson@gmail.com>
2023-02-08 21:34:59 -0500

    ben stash

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,23 @@
 
+const api_key = "aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR";
+let stateName = document.getElementById('state-code'); 
+let searchEl = document.getElementById("myBtn");
+
+const requestURL = 'https://developer.nps.gov/api/v1/parks?parkCode=&stateCode='+ stateName.value +'&api_key=aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR';
+
+function findPark() {
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+searchEl.addEventListener("click", function() {
+    findPark();
+})
+// searchEl.onclick = findPark;
+console.log(searchEl);
+console.log(stateName.value);
