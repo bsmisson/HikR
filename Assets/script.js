ddd51413e5e593b2bc89573abeb29621405ed536
Daniel <danielcruz.34776@gmail.com>
2023-02-09 20:55:58 -0500

    Cleaned files/Merge Conflicts for DANIEL

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,11 @@
+// //photos API
+// var clienId = "VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
+// var endpoint =
+//   "https://api.unsplash.com/photos/random?client_id=VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
+
+// var imageEl = document.querySelector(".adventue-pic");
+// var imageLink = document.getElementById("imglink");
+
 
 // fetch(endpoint)
 // .then(function(response){
@@ -12,24 +20,35 @@
 // });
 
 const api_key = "aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR";
-let stateName = document.getElementById('city-input'); 
 let searchEl = document.getElementById("mybtn");
-
-const requestURL = 'https://developer.nps.gov/api/v1/parks?parkCode=&stateCode='+ stateName.value +'&api_key=aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR';
+let stateName = document.getElementById('city-input');
+let parkSec = document.getElementById('park-info');
 
 function findPark() {
+    const requestURL = 'https://developer.nps.gov/api/v1/parks?parkCode=&stateCode='+ stateName.value +'&api_key=aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR';
     fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
+        const parkName =(data.data[0].fullName);
+        const parkActivities =(data.data[0].activities[0].name);
+        console.log(parkName);
+        console.log(parkActivities);
+        const heading = document.createElement('h1');
+        const activitiesList = document.createElement('ul');
+        heading.innerHTML = parkName;
+        activitiesList.innerHTML = parkActivities;
+        parkSec.appendChild(heading);
+        parkSec.appendChild(activitiesList);
     });
-}
 
-searchEl.addEventListener("click", function() {
-    findPark();
+}
+searchEl.addEventListener("click", function() { 
+    const stateName = document.getElementById('city-input').value;
+    findPark(stateName);
+    console.log(stateName);
+
 })
 // searchEl.onclick = findPark;
-console.log(searchEl);
-console.log(stateName.value);
