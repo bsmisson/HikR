1e5d4b67698391d9ec32f54de708393ac2512ff2
Maha Abdelnabi <mahaomar@gmail.com>
2023-02-09 23:22:12 -0500

    clear history function

--- Assets/script.js
+++ Assets/script.js
@@ -3,36 +3,53 @@ let searchEl = document.getElementById("mybtn");
 let stateName = document.getElementById('city-input');
 let parkSec = document.getElementById('park-info');
 
+
 function findPark() {
-    const requestURL = 'https://developer.nps.gov/api/v1/parks?parkCode=&stateCode='+ stateName.value +'&api_key=aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR';
-    fetch(requestURL)
+    
+  clearHistory()
+   
+  const requestURL =
+    "https://developer.nps.gov/api/v1/parks?parkCode=&stateCode=" +
+    stateName.value +
+    "&api_key=aRYOMb7qWKmAkeRqI32IlmrDh2cpdBE6k64szJUR";
+  fetch(requestURL)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        var numberParks= data.total
-        console.log(numberParks);
+      console.log(data);
+      var numberParks = data.total;
+      console.log(numberParks);
 
-    for (var i=0; i<numberParks; i++){
-        const parkName =(data.data[i].fullName);
+      for (var i = 0; i < numberParks; i++) {
+        const parkName = data.data[i].fullName;
         //const parkActivities =(data.data[i].activities[i].name);
         console.log(parkName);
-       // console.log(parkActivities);
-        const heading = document.createElement('h1');
-        const activitiesList = document.createElement('ul');
+        // console.log(parkActivities);
+        const heading = document.createElement("h1");
+        const activitiesList = document.createElement("ul");
         heading.innerHTML = parkName;
         //activitiesList.innerHTML = parkActivities;
         parkSec.appendChild(heading);
         parkSec.appendChild(activitiesList);
-    }
+
+       // parkSearch.innerHTML= parkName
+      }
     });
-
+  
 }
-searchEl.addEventListener("click", function() { 
-    const stateName = document.getElementById('city-input').value;
-    findPark(stateName);
-    console.log(stateName);
+//this function is to clear the history every time the user search for a new state
+ function clearHistory(){
+var clear = document.getElementById("park-info");
+while (clear.firstChild) {
+  clear.removeChild(clear.lastChild);
+}
+ }
 
+
+searchEl.addEventListener("click", function() {
+  const stateName = document.getElementById("city-input").value;
+  findPark(stateName);
+  console.log(stateName);
 })
 // searchEl.onclick = findPark;
