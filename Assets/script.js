f78c45079a01a75c40cae0b0f9af2a58e6246b4a
Daniel <danielcruz.34776@gmail.com>
2023-02-09 20:25:49 -0500

    Removed Node and Cleaned File Structures

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,16 @@
-// //photos API
-// var clienId = "VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
-// var endpoint =
-//   "https://api.unsplash.com/photos/random?client_id=VdEHKKmqqxAx4RxDgF-oMV9fdq9XsQK70OBycvs5H1Q";
-
-// var imageEl = document.querySelector(".adventue-pic");
-// var imageLink = document.getElementById("imglink");
 
 
-// fetch(endpoint)
-// .then(function(response){
-//     return response.json();
-// })
-// .then(function(jsonData){
-//     imageEl.src = jsonData.urls.regular;
-//     imageLink.setAttribute("href", jsonData.links.html);
-// })
-// .catch(function(error){
-//     console.log("error: ",error);
-// });
+
+//This is the "reponsive design"
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="Assets/images/holly-mandarich-UVyOfX3v0Ls-unsplash.jpg" alt="Modern building architecture">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+      <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+    </div>
+  </div>
+</div>
